5cd995c3a71342eafea6184ce55a89dae6de10d1
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-08-19 11:29:01 -0300

    feat(backend): add auth router

---
+++ backend/src/routes/auth.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { AuthController } from "../controllers/Auth.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
+
+const router = Router();
+const authController = new AuthController();
+
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+router.get("/profile", authMiddleware, authController.getProfile);
+
+export default router;
