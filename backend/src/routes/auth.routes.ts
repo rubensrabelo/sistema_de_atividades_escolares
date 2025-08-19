be88f81471bd24a0364fc6b16c366a4a32500648
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-08-19 19:19:39 -0300

    fix(backend): resolve import-related issues

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { AuthController } from "../controllers/Auth.controller";
+import { AuthController } from "../controllers/auth.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = Router();
